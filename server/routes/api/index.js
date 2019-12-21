c9ab9afc871ffb14e4e5172907e12e4855dbc0c3
pika-3kw <giangnam10a2@gmail.com>
2019-12-21 21:26:03 +0700

    Config babel, nodemon and API router.

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/', function (req, res, next) {
+    res.json({
+        test: true
+    })
 });
 
 module.exports = router;
