4d806b9025ba1b60df12212917bcf136e1b7adce
pika-3kw <giangnam10a2@gmail.com>
2019-12-21 21:09:04 +0700

    Define payment json structure.

--- client/src/components/Cart/components/Checkout/Checkout.js
+++ client/src/components/Cart/components/Checkout/Checkout.js
@@ -1,16 +1,37 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Button } from "react-bootstrap";
 import "./Checkout.css";
 
 const Checkout = props => {
 
+    const cart = useSelector(state => state.cart);
+
     const { total, currency } = props;
+    let create_payment_json;
+
+
+    const checkoutEvent = () => {
+        if (cart.length === 0) return;
+        let create_payment_json = {
+            intent: "sale",
+            payer: {
+                payment_method: "paypal"
+            },
+            redirect_urls: {
+                return_url: "http://return.url",
+                cancel_url: "http://cancel.url"
+            },
+            transactions: [cart]
+        };
+    }
+
     return (
         <div className='Checkout'>
             <div className="">
                 Cart total: {total} {currency}
             </div>
-            <Button variant="success">Checkout</Button>
+            <Button onClick={checkoutEvent} variant="success">Checkout</Button>
         </div>)
 }
 
