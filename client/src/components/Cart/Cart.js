afe3163d44839ff795e3d2c0ca6113a054b8f7b4
pika-3kw <namgiang10a2@gmail.com>
2019-12-21 17:32:02 +0700

    Show Cart Items.

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,10 +1,33 @@
-import React from "react";
-import { useDispatch } from 'react-redux';
-import { ListGroup } from "react-bootstrap";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { ListGroup, Image } from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Checkout from "./components/Checkout";
 import './Cart.css';
 const Cart = props => {
     const dispatch = useDispatch();
+
+    const [cartItems, setCartItems] = useState();
+
+    const cart = useSelector(state => state.cart);
+
+    useEffect(() => {
+        let { items } = cart.item_list;
+        if (items.length === 0)
+            setCartItems("Cart Empty")
+        else
+            setCartItems(items.map(item =>
+                <ListGroup.Item key={item.sku} >
+                    <Image src="https://via.placeholder.com/100x100" rounded />
+                    <div className="itemInfo">
+                        <h5>{item.name}</h5>
+                        <p>Price: {item.price} {item.currency}</p>
+                        <p>Quantity: {item.quantity}</p>
+                        <p className='Total'>Total: {item.price * item.quantity} {item.currency}</p>
+                    </div>
+                </ListGroup.Item>
+            ))
+    }, [cart])
 
     const btnClose = () => {
         dispatch({ type: "CLOSE" });
@@ -16,15 +39,11 @@ const Cart = props => {
                 <FontAwesomeIcon icon="times" />
             </span>
             <ListGroup>
-                <ListGroup.Item active >
-                    My Cart
-                </ListGroup.Item>
-                <ListGroup.Item >
-                    Link 2
-                </ListGroup.Item>
-                <ListGroup.Item onClick={() => console.log("as")} >
-                    This one is a button
-                </ListGroup.Item>
+                <ListGroup.Item active >My Cart</ListGroup.Item>
+                <div className="cartItems" style={{ overflowY: "auto" }}>
+                    {cartItems}
+                </div>
+                <Checkout total={cart.amount.total} currency={cart.amount.currenc} />
             </ListGroup>
         </div>
     )
